008bb81f7e908ae23291d5643c6351e7a5bc9432
Adriano Rodrigues <adriano@team.wellon.digital>
2022-09-13 11:01:19 -0300

    first commit

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require('@vue/cli-service');
+
+module.exports = defineConfig({
+  publicPath: '/',
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.resolve.alias.set('vue-i18n', 'vue-i18n/dist/vue-i18n.cjs.js');
+  },
+});
